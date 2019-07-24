b7ed67aee657d4950a27edeadea903f1dcd7f0a9
zaqueu lima dos santos <zaqueulima1@gmail.com>
2019-07-23 23:01:01 -0300

    Styled home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
+import Routes from "./routes";
+import GlobalStyles from "./styles/global";
+import Header from "./Components/Header";
 
 function App() {
-  return <h1>Hello from Redux</h1>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <GlobalStyles />
+      <Routes />
+    </BrowserRouter>
+  );
 }
 
 export default App;
